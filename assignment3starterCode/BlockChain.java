5520f120073f54bbff6b556e1b3a8e1094ddc622
VADALI <pvadali@amazon.com>
2018-06-05 00:17:23 -0700

    mslibs code

--- assignment3starterCode/BlockChain.java
+++ assignment3starterCode/BlockChain.java
@@ -2,86 +2,131 @@
 // You should not have all the blocks added to the block chain in memory 
 // as it would cause a memory overflow.
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class BlockChain {
+    private class BlockNode {
+        public Block b;
+        public BlockNode parent;
+        public int height;
+        // utxo pool for making a new block on top of this block
+        private UTXOPool uPool;
+
+        public BlockNode(Block b, BlockNode parent, UTXOPool uPool) {
+            this.b = b;
+            this.parent = parent;
+            this.uPool = uPool;
+            if (parent != null) {
+                height = parent.height + 1;
+            } else {
+                height = 1;
+            }
+        }
+
+        public UTXOPool getUTXOPoolCopy() {
+            return new UTXOPool(uPool);
+        }
+    }
+
+    private HashMap<ByteArrayWrapper, BlockNode> blockChain;
+    private BlockNode maxHeightNode;
+    private TransactionPool txPool;
+
     public static final int CUT_OFF_AGE = 10;
 
-    List<Block> blocks = new ArrayList<>();
-    TransactionPool transactionPool = new TransactionPool();
-    TxHandler txHandler;
-    UTXOPool utxoPool;
 
     /**
      * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
      * block
      */
     public BlockChain(Block genesisBlock) {
-        utxoPool = new UTXOPool();
-        txHandler = new TxHandler(utxoPool);
-        addCoinbaseToUTXO(genesisBlock.getCoinbase());
-        addBlock(genesisBlock);
+        blockChain = new HashMap<>();
+        UTXOPool utxoPool = new UTXOPool();
+        addCoinbaseToUTXOPool(genesisBlock, utxoPool);
+        BlockNode genesisNode = new BlockNode(genesisBlock, null, utxoPool);
+        blockChain.put(wrap(genesisBlock.getHash()), genesisNode);
+        txPool = new TransactionPool();
+        maxHeightNode = genesisNode;
     }
 
-    /** Get the maximum height block */
+    /**
+     * Get the maximum height block
+     */
     public Block getMaxHeightBlock() {
-        return blocks.get(blocks.size() - 1);
+        return maxHeightNode.b;
     }
 
-    /** Get the UTXOPool for mining a new block on top of max height block */
+    /**
+     * Get the UTXOPool for mining a new block on top of max height block
+     */
     public UTXOPool getMaxHeightUTXOPool() {
-        return this.utxoPool;
+        return maxHeightNode.getUTXOPoolCopy();
     }
 
-    /** Get the transaction pool to mine a new block */
+    /**
+     * Get the transaction pool to mine a new block
+     */
     public TransactionPool getTransactionPool() {
-        return transactionPool;
+        return txPool;
     }
 
     /**
      * Add {@code block} to the block chain if it is valid. For validity, all transactions should be
      * valid and block should be at {@code height > (maxHeight - CUT_OFF_AGE)}.
-     * 
+     * <p>
      * <p>
      * For example, you can try creating a new block over the genesis block (block height 2) if the
      * block chain height is {@code <=
      * CUT_OFF_AGE + 1}. As soon as {@code height > CUT_OFF_AGE + 1}, you cannot create a new block
      * at height 2.
-     * 
+     *
      * @return true if block is successfully added
      */
     public boolean addBlock(Block block) {
-        if (blocks.size() > CUT_OFF_AGE + 1)
+        byte[] prevBlockHash = block.getPrevBlockHash();
+        if (prevBlockHash == null)
             return false;
-        Transaction[] transactions = new Transaction[block.getTransactions().size()];
-        Transaction[] validTx = txHandler.handleTxs(block.getTransactions().toArray(transactions));
-        if (validTx.length == block.getTransactions().size()) {
-            addCoinbaseToUTXO(block.getCoinbase());
-            blocks.add(block);
-            return true;
+        BlockNode parentBlockNode = blockChain.get(wrap(prevBlockHash));
+        if (parentBlockNode == null) {
+            return false;
         }
-        return false;
+        TxHandler handler = new TxHandler(parentBlockNode.getUTXOPoolCopy());
+        Transaction[] txs = block.getTransactions().toArray(new Transaction[0]);
+        Transaction[] validTxs = handler.handleTxs(txs);
+        if (validTxs.length != txs.length) {
+            return false;
+        }
+        int proposedHeight = parentBlockNode.height + 1;
+        if (proposedHeight <= maxHeightNode.height - CUT_OFF_AGE) {
+            return false;
+        }
+        UTXOPool utxoPool = handler.getUTXOPool();
+        addCoinbaseToUTXOPool(block, utxoPool);
+        BlockNode node = new BlockNode(block, parentBlockNode, utxoPool);
+        blockChain.put(wrap(block.getHash()), node);
+        if (proposedHeight > maxHeightNode.height) {
+            maxHeightNode = node;
+        }
+        return true;
     }
 
-    /** Add a transaction to the transaction pool */
+    /**
+     * Add a transaction to the transaction pool
+     */
     public void addTransaction(Transaction tx) {
-        for (Block block : blocks) {
-            for(Transaction transaction : block.getTransactions()) {
-                if (transaction == tx)
-                    return;
-            }
-        }
-        this.transactionPool.addTransaction(tx);
+        txPool.addTransaction(tx);
     }
 
-    private void addCoinbaseToUTXO(Transaction coinbase) {
+    private void addCoinbaseToUTXOPool(Block block, UTXOPool utxoPool) {
+        Transaction coinbase = block.getCoinbase();
         for (int i = 0; i < coinbase.numOutputs(); i++) {
+            Transaction.Output out = coinbase.getOutput(i);
             UTXO utxo = new UTXO(coinbase.getHash(), i);
-            this.utxoPool.addUTXO(utxo, coinbase.getOutput(i));
+            utxoPool.addUTXO(utxo, out);
         }
     }
 
+    private static ByteArrayWrapper wrap(byte[] arr) {
+        return new ByteArrayWrapper(arr);
+    }
 }
